73e282c5d0f1f713acfbb3826abb491533ce2321
andyogaga <andrewo@shleeq.com>
2020-10-13 10:02:29 +0100

    adds sending emails

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,12 +4,9 @@ import inquiryModel from "../models/inquiry.model";
 import SendGridMail from "@sendgrid/mail";
 import userModel from "../models/user.model";
 
-SendGridMail.setApiKey(
-  "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"
-);
+SendGridMail.setApiKey(process.env.SENDGRID_API);
 
 export const createLead = async (req, res, next) => {
-  console.log(req.body);
   try {
     const {
       fname = "",
@@ -66,11 +63,33 @@ export const createLead = async (req, res, next) => {
     });
     await SendGridMail.send({
       from: "no-reply@tractrac.co",
-      to: email,
-      templateId: "d-c05f32b44f1e4babbbb655cde60b49ea",
+      to: "tractracnigeria@gmail.com",
+      subject: "Tractrac Inquiry made",
+      text: "A new inquiry was made into Tractrac",
+      html: `<div>
+      <p>Hi,</p>
+      <p>A new inquiry has been made, see below for details</p>
+      <p>Firstname: ${fname}</p>,
+      <p>Lastname: ${lname}</p>,
+      <p>Gender: ${gender}</p>,
+      <p>Business type: ${businessType}</p>,
+      <p>Message: ${message}</p>,
+      <p>Email: ${email}</p>,
+      <p>Phone: ${phone}</p>,
+      <p>Phone2: ${phoneNumber}</p>,
+      <p>Type of Lead: ${leadType}</p>,
+      <p>Number of tractors requested: ${tractorAmount}</p>,
+      <p>Number of Operators: ${operatorAmount}</p>,
+      <p>Tractor brands: ${tractorBrands}</p>,
+      <p>State: ${state}</p>,
+      <p>Lga: ${lga}</p>,
+      <p>Size of farm: ${farmSize}</p>,
+      <p>Recommendations: ${recommendations}</p>,
+      <div>
+      `,
     });
     return res.status(201).json({
-      message: "Created  successfully",
+      message: "Submitted  successfully",
     });
   } catch (err) {
     return next({
