17462f9eed5f043c1cdbcd14bba5db227b4d560e
cheksman <petero@shleeq.com>
2020-11-03 16:43:33 +0100

    Added Mailchimp controller

--- src/routes/routerAPI.js
+++ src/routes/routerAPI.js
@@ -1,10 +1,15 @@
 /* eslint-disable max-len */
 import express from "express";
 import { createLead } from "../controllers/user.controllers";
+import { subscribeContactToMailchimp } from './../controllers/mailchimp.controllers';
 
 const router = express.Router();
 
 // User Routes
 router.post("/user/create", createLead);
+
+// Mailchimp Routes
+router.post('/mailchimp/subscribe', subscribeContactToMailchimp);
+
 
 export default router;
