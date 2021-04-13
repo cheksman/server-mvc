92b0d36b9b4d21458b9677a54a73058d3c8af5ea
baronchibuikem <baronchibuike@gmail.com>
2021-04-13 22:48:58 +0100

    update: ...

--- src/controllers/forgotPassword.controller.js
+++ src/controllers/forgotPassword.controller.js
@@ -1,37 +1,40 @@
-import {findUser} from "../services/user.services"
-const twilio = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+import { findUser } from "../services/user.services";
+const twilio = require("twilio")(
+  process.env.ACCOUNT_SID,
+  process.env.AUTH_TOKEN
+);
 
 export const sendOTP = async (req, res, next) => {
- 
- try{
+  console.log("running");
+
+  try {
     const { phoneNumber } = req.body;
-    const updatedNumber = phoneNumber.replace(0, '+234')
+    const updatedNumber = phoneNumber.replace(0, "+234");
     let user = null;
 
     // check if a user with the phoneNumber entered already exist
-    user = await findUser(phoneNumber); 
-    if (!user || phoneNumber === '') {
+    user = await findUser(phoneNumber);
+    if (!user || phoneNumber === "") {
       return res.status(403).json({
         message: "Phone number is not registered",
       });
     }
     //if it exist, send an otp to the phone number entered
-          else{
-              twilio.verify.services(process.env.SERVICE_ID).verifications.create({
-                  to: updatedNumber,
-                  channel: 'sms'            
-                })
-                .then((data) => {
-                    res.status(200).send("Successful! Please check your messages.")
-                })
-          }
-          
-        } catch (error) {
-          return next({
-            message: "Error, please try again",
-            error: error,
-          });
-        }
-
- }
-
+    else {
+      twilio.verify
+        .services(process.env.SERVICE_ID)
+        .verifications.create({
+          to: updatedNumber,
+          channel: "sms",
+        })
+        .then((data) => {
+          res.status(200).send("Successful! Please check your messages.");
+        });
+    }
+  } catch (error) {
+    return next({
+      message: "Error, please try again",
+      error: error,
+    });
+  }
+};
